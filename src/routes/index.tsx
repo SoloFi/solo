f8f8caf322d63c8d1909f9480f0f20e5c83f2dc7
Zach Legros <legroszach3921@gmail.com>
2024-06-13 21:17:54 -0400

    Fix build

---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: Index,
+})
+
+function Index() {
+  return (
+    <div className="p-2">
+      <h3>Welcome Home!</h3>
+      <Button>Click me!</Button>
+    </div>
+  )
+}
