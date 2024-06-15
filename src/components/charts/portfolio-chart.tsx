7b5171bc0289ea798802dfd4f0a271878c114098
Zach Legros <legroszach3921@gmail.com>
2024-06-15 17:35:38 -0400

    refactor: Optimize usePortfolioChartData hook

--- src/components/charts/portfolio-chart.tsx
+++ src/components/charts/portfolio-chart.tsx
@@ -4,56 +4,88 @@ import {
   LastPriceAnimationMode,
   LineStyle,
   type CandlestickData,
+  type DeepPartial,
+  type ISeriesApi,
   type MouseEventParams,
   type Time,
+  type TimeChartOptions,
   type UTCTimestamp,
 } from "lightweight-charts";
-import { useEffect, useRef, useCallback, useLayoutEffect, memo } from "react";
+import { useRef, useCallback, useEffect, useState } from "react";
 import useChartOptions from "./useChartOptions";
 import type { CostBasisData } from "@/api/portfolio";
 import colors from "tailwindcss/colors";
-import { useTheme } from "@/components/theme-provider";
+import { useTheme, type Theme } from "@/components/theme-provider";
+import ChartTooltip from "./chart-tooltip";
+import { dayjs, hexTransp, usd } from "@/lib/utils";
 
-export const PortfolioChart = memo(
-  (props: {
-    data: CandlestickData<UTCTimestamp>[];
-    costBasisData: CostBasisData[];
-    type: "area" | "candlestick";
-    height?: number;
-    onTooltipChange?: (tooltip: { time: UTCTimestamp; value: number }) => void;
-  }) => {
-    const { data, costBasisData, type, height = 400, onTooltipChange } = props;
-    const { theme } = useTheme();
-    const chartContainerRef = useRef<HTMLDivElement>(null);
-    const { options } = useChartOptions({
-      width: chartContainerRef.current?.clientWidth,
-      height,
-    });
+export const PortfolioChart = (props: {
+  data: CandlestickData<UTCTimestamp>[];
+  costBasisData: CostBasisData[];
+  type: "area" | "candlestick";
+  height?: number;
+}) => {
+  const { data, costBasisData, type, height } = props;
+  const { theme } = useTheme();
+  const [tooltip, setTooltip] = useState<{ time: UTCTimestamp; value: number } | null>(
+    null,
+  );
+  const parentContainerRef = useRef<HTMLDivElement | null>(null);
+  const chartContainerRef = useRef<HTMLDivElement | null>(null);
+  const chartRef = useRef<ReturnType<typeof createChart>>();
+  const portfolioSeriesRef = useRef<
+    ISeriesApi<"Area", Time> | ISeriesApi<"Candlestick">
+  >();
 
-    const getLastBar = useCallback(() => {
-      const lastData = data[data.length - 1];
-      return {
-        time: lastData.time,
-        value: lastData.close,
-      };
-    }, [data]);
+  const { options } = useChartOptions();
 
-    useLayoutEffect(() => {
-      onTooltipChange?.(getLastBar());
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  const getLastBar = useCallback(() => {
+    const lastData = data[data.length - 1];
+    return {
+      time: lastData.time,
+      value: lastData.close,
+    };
+  }, [data]);
 
-    useEffect(() => {
-      if (!chartContainerRef.current) return;
-      const handleResize = () => {
-        chart.applyOptions({ width: chartContainerRef.current?.clientWidth });
-      };
+  const handleUpdateTooltip = useCallback(
+    (param: MouseEventParams<Time>) => {
+      if (!portfolioSeriesRef.current) return;
+      let validCrosshairPoint = true;
+      if (!param || !param.point) validCrosshairPoint = false;
+      if (param.point && (param.point.x < 0 || param.point.y < 0))
+        validCrosshairPoint = false;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const bar: any = validCrosshairPoint
+        ? param.seriesData.get(portfolioSeriesRef.current)
+        : getLastBar();
+      if (!bar) return;
+      const time = bar.time;
+      const price = bar.value !== undefined ? bar.value : bar.close;
+      setTooltip({ time, value: price });
+    },
+    [getLastBar],
+  );
+
+  const handleCreateChart = useCallback(
+    (params: {
+      data: CandlestickData<UTCTimestamp>[];
+      costBasisData: CostBasisData[];
+      type: "area" | "candlestick";
+      options: DeepPartial<TimeChartOptions>;
+      theme: Theme;
+    }) => {
+      const { data, costBasisData, options, theme, type } = params;
+      if (chartRef.current || !chartContainerRef.current) return;
       const chart = createChart(chartContainerRef.current, options);
+      chartRef.current = chart;
 
       let portfolioSeries;
       if (type === "area") {
         portfolioSeries = chart.addAreaSeries({
           lastPriceAnimation: LastPriceAnimationMode.Continuous,
+          lineColor: colors.sky[500],
+          topColor: hexTransp(colors.sky[500], 50),
+          bottomColor: hexTransp(colors.sky[500], 5),
         });
         portfolioSeries.setData(
           data.map((entry) => ({ time: entry.time, value: entry.close })),
@@ -62,41 +94,70 @@ export const PortfolioChart = memo(
         portfolioSeries = chart.addCandlestickSeries();
         portfolioSeries.setData(data);
       }
+      portfolioSeriesRef.current = portfolioSeries;
       const costBasisSeries = chart.addLineSeries({
         color: colors.zinc[theme === "dark" ? 400 : 500],
         lineWidth: 2,
         lineStyle: LineStyle.LargeDashed,
       });
       costBasisSeries.setData(costBasisData);
-
       chart.timeScale().fitContent();
+      setTooltip(getLastBar());
+    },
+    [getLastBar],
+  );
 
-      const updateTooltip = (param: MouseEventParams<Time>) => {
-        let validCrosshairPoint = true;
-        if (!param || !param.point) validCrosshairPoint = false;
-        if (param.point && (param.point.x < 0 || param.point.y < 0))
-          validCrosshairPoint = false;
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const bar: any = validCrosshairPoint
-          ? param.seriesData.get(portfolioSeries)
-          : getLastBar();
-        if (!bar) return;
-        const time = bar.time;
-        const price = bar.value !== undefined ? bar.value : bar.close;
-        onTooltipChange?.({ time, value: price });
-      };
-      chart.subscribeCrosshairMove(updateTooltip);
+  const initContainerRef = useCallback(
+    (node: HTMLDivElement) => {
+      if (!node) return;
+      chartContainerRef.current = node;
+      handleCreateChart({ data, costBasisData, options, theme, type });
+    },
+    [costBasisData, data, handleCreateChart, options, theme, type],
+  );
 
-      window.addEventListener("resize", handleResize);
-      return () => {
-        console.log("cleanup");
-        window.removeEventListener("resize", handleResize);
-        chart.unsubscribeCrosshairMove(updateTooltip);
-        chart.remove();
-      };
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [costBasisData, data, height, options, theme, type]);
+  const addChartEventListeners = useCallback(() => {
+    if (!chartRef.current) return;
+    chartRef.current.subscribeCrosshairMove(handleUpdateTooltip);
+  }, [handleUpdateTooltip]);
 
-    return <div ref={chartContainerRef} />;
-  },
-);
+  const removeChartEventListeners = useCallback(() => {
+    if (!chartRef.current) return;
+    chartRef.current.unsubscribeCrosshairMove(handleUpdateTooltip);
+  }, [handleUpdateTooltip]);
+
+  useEffect(() => {
+    const chart = chartRef.current;
+    if (!chart) return;
+    try {
+      chart.remove();
+    } catch (e) {
+      null;
+    }
+    chartRef.current = undefined;
+    handleCreateChart({ data, costBasisData, options, theme, type });
+    addChartEventListeners();
+    return removeChartEventListeners;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [theme, type]);
+
+  return (
+    <div
+      ref={parentContainerRef}
+      className="flex w-full overflow-hidden relative"
+      style={{ height: height ?? 400 }}
+    >
+      <div className="absolute w-full h-full">
+        <div ref={initContainerRef} />
+        {tooltip && tooltip.time && tooltip.value ? (
+          <ChartTooltip
+            time={dayjs(tooltip.time * 1000)
+              .utc()
+              .format("MMMM D, YYYY")}
+            value={usd(tooltip.value)}
+          />
+        ) : null}
+      </div>
+    </div>
+  );
+};
--- src/components/portfolio/usePortfolioChartData.tsx
+++ src/components/portfolio/usePortfolioChartData.tsx
@@ -1,88 +1,91 @@
 import type { Portfolio } from "@/api/portfolio";
 import type { CandlestickData } from "@/api/symbol";
 import type { UTCTimestamp } from "lightweight-charts";
+import { useMemo } from "react";
 
 // Hook to transform portfolio holdings and symbol chart data into a format that can be used by the chart component
 export const usePortfolioChartData = (props: {
   holdings?: Portfolio["holdings"];
-  data?: Record<string, CandlestickData[]>;
+  data?: Record<string, CandlestickData[] | undefined>;
 }): {
   candlestickData: CandlestickData[];
   costBasisData: { time: UTCTimestamp; value: number }[];
 } => {
   const { holdings, data } = props;
-  if (!holdings || !data) {
-    return { candlestickData: [], costBasisData: [] };
-  }
 
-  const holdingsData = holdings.map(({ symbol, buys, sells }) => {
-    const symbolData = data[symbol];
-    if (!symbolData) {
-      return { holdingOHLC: [], costBasisOverTime: [] };
-    }
-    const timeline = symbolData.map((entry) => entry.time);
-    const sharesOverTime = timeline.map((time) => {
-      const buyShares = buys
-        .filter((buy) => buy.time <= time)
-        .reduce((acc, buy) => acc + buy.quantity, 0);
-      const sellShares = (sells || [])
-        .filter((sell) => sell.time <= time)
-        .reduce((acc, sell) => acc + sell.quantity, 0);
-      return buyShares - sellShares;
+  const { candlestickData, costBasisData } = useMemo(() => {
+    if (!holdings || !data) return { candlestickData: [], costBasisData: [] };
+    const holdingsData = holdings.map(({ symbol, buys, sells }) => {
+      const symbolData = data[symbol];
+      if (!symbolData) {
+        return { holdingOHLC: [], costBasisOverTime: [] };
+      }
+      const timeline = symbolData.map((entry) => entry.time);
+      const sharesOverTime = timeline.map((time) => {
+        const buyShares = buys
+          .filter((buy) => buy.time <= time)
+          .reduce((acc, buy) => acc + buy.quantity, 0);
+        const sellShares = (sells || [])
+          .filter((sell) => sell.time <= time)
+          .reduce((acc, sell) => acc + sell.quantity, 0);
+        return buyShares - sellShares;
+      });
+      const costBasisOverTime = timeline.map((time) => {
+        const buyCost = buys
+          .filter((buy) => buy.time <= time)
+          .reduce((acc, buy) => acc + buy.price * buy.quantity, 0);
+        const sellCost = (sells || [])
+          .filter((sell) => sell.time <= time)
+          .reduce((acc, sell) => acc + sell.price * sell.quantity, 0);
+        return buyCost - sellCost;
+      });
+      const holdingOHLC = timeline.map((_, index) => {
+        const { open, high, low, close } = symbolData[index];
+        return {
+          time: timeline[index],
+          open: open * sharesOverTime[index],
+          high: high * sharesOverTime[index],
+          low: low * sharesOverTime[index],
+          close: close * sharesOverTime[index],
+          cost: costBasisOverTime[index],
+        };
+      });
+      return { holdingOHLC, costBasisOverTime };
     });
-    const costBasisOverTime = timeline.map((time) => {
-      const buyCost = buys
-        .filter((buy) => buy.time <= time)
-        .reduce((acc, buy) => acc + buy.price * buy.quantity, 0);
-      const sellCost = (sells || [])
-        .filter((sell) => sell.time <= time)
-        .reduce((acc, sell) => acc + sell.price * sell.quantity, 0);
-      return buyCost - sellCost;
+
+    // Combine the OHLC data of all holdings using the timeline with the highest resolution
+    const combinedTimeline = Array.from(
+      new Set(
+        holdingsData.flatMap(({ holdingOHLC }) => holdingOHLC.map((entry) => entry.time)),
+      ),
+    ).sort();
+
+    const combinedData = combinedTimeline.map((time) => {
+      const combinedEntry = holdingsData
+        .map(({ holdingOHLC }) => holdingOHLC.find((entry) => entry.time === time))
+        .filter((entry) => entry);
+      const open = combinedEntry.reduce((acc, entry) => acc + (entry?.open ?? 0), 0);
+      const high = combinedEntry.reduce((acc, entry) => acc + (entry?.high ?? 0), 0);
+      const low = combinedEntry.reduce((acc, entry) => acc + (entry?.low ?? 0), 0);
+      const close = combinedEntry.reduce((acc, entry) => acc + (entry?.close ?? 0), 0);
+      const cost = combinedEntry.reduce((acc, entry) => acc + (entry?.cost ?? 0), 0);
+      return { time, open, high, low, close, cost };
     });
-    const holdingOHLC = timeline.map((_, index) => {
-      const { open, high, low, close } = symbolData[index];
-      return {
-        time: timeline[index],
-        open: open * sharesOverTime[index],
-        high: high * sharesOverTime[index],
-        low: low * sharesOverTime[index],
-        close: close * sharesOverTime[index],
-        cost: costBasisOverTime[index],
-      };
-    });
-    return { holdingOHLC, costBasisOverTime };
-  });
 
-  // Combine the OHLC data of all holdings using the timeline with the highest resolution
-  const combinedTimeline = Array.from(
-    new Set(
-      holdingsData.flatMap(({ holdingOHLC }) => holdingOHLC.map((entry) => entry.time)),
-    ),
-  ).sort();
+    return {
+      candlestickData: combinedData.map(({ open, high, low, close, time }) => ({
+        open,
+        high,
+        low,
+        close,
+        time,
+      })),
+      costBasisData: combinedData.map(({ cost, time }) => ({
+        value: cost,
+        time,
+      })),
+    };
+  }, [data, holdings]);
 
-  const combinedData = combinedTimeline.map((time) => {
-    const combinedEntry = holdingsData
-      .map(({ holdingOHLC }) => holdingOHLC.find((entry) => entry.time === time))
-      .filter((entry) => entry);
-    const open = combinedEntry.reduce((acc, entry) => acc + (entry?.open ?? 0), 0);
-    const high = combinedEntry.reduce((acc, entry) => acc + (entry?.high ?? 0), 0);
-    const low = combinedEntry.reduce((acc, entry) => acc + (entry?.low ?? 0), 0);
-    const close = combinedEntry.reduce((acc, entry) => acc + (entry?.close ?? 0), 0);
-    const cost = combinedEntry.reduce((acc, entry) => acc + (entry?.cost ?? 0), 0);
-    return { time, open, high, low, close, cost };
-  });
-
-  return {
-    candlestickData: combinedData.map(({ open, high, low, close, time }) => ({
-      open,
-      high,
-      low,
-      close,
-      time,
-    })),
-    costBasisData: combinedData.map(({ cost, time }) => ({
-      value: cost,
-      time,
-    })),
-  };
+  return { candlestickData, costBasisData };
 };
