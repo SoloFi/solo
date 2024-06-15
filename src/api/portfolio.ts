e5297fcf857fc9f86d2f11884ed536e5c98787b5
Zach Legros <legroszach3921@gmail.com>
2024-06-15 15:15:21 -0400

    feat: Add portfolio API and mock data

---
+++ src/api/portfolio.ts
@@ -0,0 +1,32 @@
+import type { UTCTimestamp } from "lightweight-charts";
+
+export type Portfolio = {
+  id: string;
+  name: string;
+  holdings: {
+    symbol: string;
+    buys: {
+      time: number;
+      quantity: number;
+      price: number;
+    }[];
+    sells?: {
+      time: number;
+      quantity: number;
+      price: number;
+    }[];
+  }[];
+};
+
+export type CostBasisData = {
+  time: UTCTimestamp;
+  value: number;
+};
+
+export const getPortfolio = async () => {
+  const response = await fetch("/api/portfolio");
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return (await response.json()) as Portfolio;
+};
