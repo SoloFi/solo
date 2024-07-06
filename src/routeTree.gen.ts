f4b7125fa4dbb464397cbf78f322e41a16a76cb5
Zachary Legros <legroszach3921@gmail.com>
2024-07-06 17:21:44 -0400

    Updated route gen

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -11,9 +11,27 @@
 // Import Routes
 
 import { Route as rootRoute } from './routes/__root'
+import { Route as SignUpImport } from './routes/signUp'
+import { Route as SignInImport } from './routes/signIn'
+import { Route as layoutImport } from './routes/__layout'
 import { Route as IndexImport } from './routes/index'
 
 // Create/Update Routes
+
+const SignUpRoute = SignUpImport.update({
+  path: '/signUp',
+  getParentRoute: () => rootRoute,
+} as any)
+
+const SignInRoute = SignInImport.update({
+  path: '/signIn',
+  getParentRoute: () => rootRoute,
+} as any)
+
+const layoutRoute = layoutImport.update({
+  id: '/__layout',
+  getParentRoute: () => rootRoute,
+} as any)
 
 const IndexRoute = IndexImport.update({
   path: '/',
@@ -31,12 +49,37 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof IndexImport
       parentRoute: typeof rootRoute
     }
+    '/__layout': {
+      id: '/__layout'
+      path: ''
+      fullPath: ''
+      preLoaderRoute: typeof layoutImport
+      parentRoute: typeof rootRoute
+    }
+    '/signIn': {
+      id: '/signIn'
+      path: '/signIn'
+      fullPath: '/signIn'
+      preLoaderRoute: typeof SignInImport
+      parentRoute: typeof rootRoute
+    }
+    '/signUp': {
+      id: '/signUp'
+      path: '/signUp'
+      fullPath: '/signUp'
+      preLoaderRoute: typeof SignUpImport
+      parentRoute: typeof rootRoute
+    }
   }
 }
 
 // Create and export the route tree
 
-export const routeTree = rootRoute.addChildren({ IndexRoute })
+export const routeTree = rootRoute.addChildren({
+  IndexRoute,
+  SignInRoute,
+  SignUpRoute,
+})
 
 /* prettier-ignore-end */
 
@@ -46,11 +89,23 @@ export const routeTree = rootRoute.addChildren({ IndexRoute })
     "__root__": {
       "filePath": "__root.tsx",
       "children": [
-        "/"
+        "/",
+        "/__layout",
+        "/signIn",
+        "/signUp"
       ]
     },
     "/": {
       "filePath": "index.tsx"
+    },
+    "/__layout": {
+      "filePath": "__layout.tsx"
+    },
+    "/signIn": {
+      "filePath": "signIn.tsx"
+    },
+    "/signUp": {
+      "filePath": "signUp.tsx"
     }
   }
 }
