a47b9c0bca92b2aab422e63d83d22b58cb9fb2ec
Zachary Legros <legroszach3921@gmail.com>
2024-07-06 17:18:58 -0400

    Added auth hooks

---
+++ src/components/auth-provider.tsx
@@ -0,0 +1,67 @@
+import { axios } from "@/query/axios";
+import {
+  createContext,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+  type ReactNode,
+} from "react";
+
+export interface AuthContextType {
+  token: string | null;
+  signIn: (email: string, password: string) => Promise<void>;
+  signUp: (email: string, password: string, accessKey: string) => Promise<string>;
+}
+export const AuthContext = createContext<AuthContextType>({} as AuthContextType);
+
+const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [token, setToken] = useState<string | null>(localStorage.getItem("token"));
+
+  const signIn = useCallback(async (email: string, password: string) => {
+    const res = await axios.post("/signIn", { email, password });
+    const token = res.data.token;
+    if (!token) throw new Error(res.data.message);
+    setToken(token);
+    return token as string | undefined;
+  }, []);
+
+  const signUp = useCallback(
+    async (email: string, password: string, accessKey: string) => {
+      const res = await axios.post(
+        "/signUp",
+        { email, password },
+        {
+          headers: {
+            Authorization: `Bearer ${accessKey}`,
+          },
+        },
+      );
+      return res.data as string;
+    },
+    [],
+  );
+
+  useEffect(() => {
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+      localStorage.setItem("token", token);
+    } else {
+      delete axios.defaults.headers.common["Authorization"];
+      localStorage.removeItem("token");
+    }
+  }, [token]);
+
+  const contextValue = useMemo(
+    () => ({
+      token,
+      signIn,
+      signUp,
+    }),
+    [signIn, signUp, token],
+  );
+
+  return <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
