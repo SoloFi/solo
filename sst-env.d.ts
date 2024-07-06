adaa81f5f911ddba43e57d4a5ad58c01a4fd6d3b
Zachary Legros <legroszach3921@gmail.com>
2024-07-06 17:17:52 -0400

    Updated dependencies and scripts

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -3,10 +3,26 @@
 import "sst"
 declare module "sst" {
   export interface Resource {
+    APIToken: {
+      type: "sst.sst.Secret"
+      value: string
+    }
     Hono: {
       name: string
       type: "sst.aws.Function"
       url: string
+    }
+    JWTSecret: {
+      type: "sst.sst.Secret"
+      value: string
+    }
+    Solo: {
+      type: "sst.aws.StaticSite"
+      url: string
+    }
+    Users: {
+      name: string
+      type: "sst.aws.Dynamo"
     }
   }
 }
