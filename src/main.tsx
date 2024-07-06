2dbcb8dbc0c798b16e565fb8615645e71ccdc00b
Zachary Legros <legroszach3921@gmail.com>
2024-07-06 17:21:36 -0400

    Router with context

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,13 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { ThemeProvider } from "@/components/theme-provider";
 import { routeTree } from "./routeTree.gen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createServer } from "miragejs";
-import { API_BASE_URL } from "@/api/query/utils";
+import { useAuth } from "./components/auth";
+import AuthProvider from "./components/auth-provider";
 
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+  context: { auth: undefined! },
+});
 const queryClient = new QueryClient();
 
 declare module "@tanstack/react-router" {
@@ -17,15 +20,23 @@ declare module "@tanstack/react-router" {
   }
 }
 
-if (import.meta.env.DEV) {
-  createServer({
-    routes() {
-      this.get("/api/portfolio", () =>
-        import("@/api/mockData/portfolio.json").then((res) => res.default),
-      );
-      this.passthrough(`${API_BASE_URL}/**`);
-    },
-  });
+function InnerApp() {
+  const auth = useAuth();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} context={{ auth }} />
+    </QueryClientProvider>
+  );
+}
+
+export function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <AuthProvider>
+        <InnerApp />
+      </AuthProvider>
+    </ThemeProvider>
+  );
 }
 
 const rootElement = document.getElementById("root")!;
@@ -33,11 +44,7 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-        </QueryClientProvider>
-      </ThemeProvider>
+      <App />
     </StrictMode>,
   );
 }
