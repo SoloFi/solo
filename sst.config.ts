73da858a5e0b4f52312b5cd3cc050f31b0d6ef9f
Zachary Legros <legroszach3921@gmail.com>
2024-07-06 17:17:13 -0400

    Added dynamo db, frontend and secrets to sst config

--- sst.config.ts
+++ sst.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
@@ -9,13 +10,36 @@ export default $config({
     };
   },
   async run() {
+    const jwtSecret = new sst.Secret("JWTSecret");
+    const apiToken = new sst.Secret("APIToken");
+
+    const usersTable = new sst.aws.Dynamo("Users", {
+      fields: {
+        email: "string",
+      },
+      primaryIndex: { hashKey: "email" },
+    });
+
     const hono = new sst.aws.Function("Hono", {
       url: true,
       handler: "src/api/index.handler",
+      link: [usersTable, jwtSecret, apiToken],
+      runtime: "nodejs20.x",
+    });
+
+    new sst.aws.StaticSite("Solo", {
+      build: {
+        command: "npm run build",
+        output: "dist",
+      },
+      environment: {
+        VITE_API_BASE_URL: hono.url,
+      },
     });
 
     return {
       api: hono.url,
+      usersTable,
     };
   },
 });
