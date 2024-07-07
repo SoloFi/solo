b1aa7f2d2f5fd37590155d4c2998447ec1557540
Zachary Legros <legroszach3921@gmail.com>
2024-07-06 22:28:40 -0400

    Changed shell sidebar width

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -7,7 +7,7 @@ import { Bell, LineChart } from "lucide-react";
 
 export const Route = createFileRoute("/_app")({
   component: () => (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[250px_1fr]">
       <div className="hidden border-r bg-background md:block">
         <div className="flex flex-col h-full max-h-screen gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
