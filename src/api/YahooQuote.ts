9060ed8cc13b4e276d9c4446b42c61e28c67a1be
Zachary Legros <legroszach3921@gmail.com>
2024-07-07 16:32:25 -0400

    Added latest quote api query

--- src/api/YahooQuote.ts
+++ src/api/YahooQuote.ts
@@ -6,6 +6,30 @@ class YahooQuote {
   private region = "US";
   private lang = "en-US";
 
+  async getLatestQuote(symbol: string) {
+    const url: URL = new URL(`${this.baseUrl}${symbol}`);
+    url.searchParams.append("region", this.region);
+    url.searchParams.append("lang", this.lang);
+    url.searchParams.append("includePrePost", "true");
+    url.searchParams.append("interval", "1d");
+    const response = await fetch(url.toString());
+    if (!response.ok) {
+      return response;
+    }
+    const data = await response.json();
+    const timestamp = data?.chart?.result?.[0]?.timestamp as UTCTimestamp[];
+    const quote = data?.chart?.result?.[0]?.indicators?.quote[0] as Quote;
+    if (!timestamp || !quote) throw new Error();
+    const latestQuote: CandlestickData = {
+      time: timestamp[timestamp.length - 1],
+      open: quote.open[quote.open.length - 1],
+      high: quote.high[quote.high.length - 1],
+      low: quote.low[quote.low.length - 1],
+      close: quote.close[quote.close.length - 1],
+    };
+    return latestQuote;
+  }
+
   async getCandlestickData(params: {
     symbol: string;
     interval: string;
@@ -32,7 +56,7 @@ class YahooQuote {
     const data = await response.json();
     const timestamp = data?.chart?.result?.[0]?.timestamp as UTCTimestamp[];
     const quote = data?.chart?.result?.[0]?.indicators?.quote[0] as Quote;
-    if (!timestamp || !quote) throw new Error("Invalid data");
+    if (!timestamp || !quote) throw new Error();
     const candlestickData: CandlestickData[] = timestamp.map((time, index) => ({
       time: time,
       open: quote.open[index],
