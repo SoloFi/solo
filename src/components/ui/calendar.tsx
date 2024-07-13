67516a4a89b4bcb61637ee39d40eac43581a6cea
Zachary Legros <legroszach3921@gmail.com>
2024-07-13 16:50:29 -0400

    Added select and calendar

---
+++ src/components/ui/calendar.tsx
@@ -0,0 +1,165 @@
+import * as React from "react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
+import { DayPicker } from "react-day-picker";
+
+import { cn, dayjs } from "@/lib/utils";
+import { buttonVariants } from "./button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "./select";
+
+export type CalendarProps = React.ComponentProps<typeof DayPicker>;
+
+function Calendar({
+  className,
+  classNames,
+  showOutsideDays = true,
+  ...props
+}: CalendarProps) {
+  const { onMonthChange } = props;
+
+  return (
+    <DayPicker
+      showOutsideDays={showOutsideDays}
+      className={cn("p-3", className)}
+      classNames={{
+        months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
+        month: "space-y-4",
+        caption: "flex justify-center pt-1 relative items-center",
+        caption_label: "text-sm font-medium",
+        nav: "space-x-1 flex items-center",
+        nav_button: cn(
+          buttonVariants({ variant: "outline" }),
+          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100",
+        ),
+        nav_button_previous: "absolute left-1",
+        nav_button_next: "absolute right-1",
+        table: "w-full border-collapse space-y-1",
+        head_row: "flex",
+        head_cell: "text-muted-foreground rounded-md w-8 font-normal text-[0.8rem]",
+        row: "flex w-full mt-2",
+        cell: cn(
+          "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected].day-range-end)]:rounded-r-md",
+          props.mode === "range"
+            ? "[&:has(>.day-range-end)]:rounded-r-md [&:has(>.day-range-start)]:rounded-l-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md"
+            : "[&:has([aria-selected])]:rounded-md",
+        ),
+        day: cn(
+          buttonVariants({ variant: "ghost" }),
+          "h-8 w-8 p-0 font-normal aria-selected:opacity-100",
+        ),
+        day_range_start: "day-range-start",
+        day_range_end: "day-range-end",
+        day_selected:
+          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
+        day_today: "bg-accent text-accent-foreground",
+        day_outside:
+          "day-outside text-muted-foreground opacity-50  aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
+        day_disabled: "text-muted-foreground opacity-50",
+        day_range_middle: "aria-selected:bg-accent aria-selected:text-accent-foreground",
+        day_hidden: "invisible",
+        ...classNames,
+      }}
+      components={{
+        IconLeft: () => <ChevronLeftIcon className="h-4 w-4" />,
+        IconRight: () => <ChevronRightIcon className="h-4 w-4" />,
+        Caption: ({ ...props }) => {
+          const { displayMonth } = props;
+          const currentMonth = dayjs(displayMonth).format("MMMM");
+          const currentYear = dayjs(displayMonth).format("YYYY");
+          const months = React.useMemo(
+            () => [
+              "January",
+              "February",
+              "March",
+              "April",
+              "May",
+              "June",
+              "July",
+              "August",
+              "September",
+              "October",
+              "November",
+              "December",
+            ],
+            [],
+          );
+          // From 1970 to current year
+          const years = React.useMemo(() => {
+            const currentYear = dayjs().year();
+            return Array.from(
+              { length: currentYear - 1970 + 1 },
+              (_, i) => 1970 + i,
+            ).reverse();
+          }, []);
+
+          return (
+            <div className="flex gap-2">
+              <Select
+                value={currentMonth}
+                onValueChange={(month) => {
+                  const newMonth = dayjs(displayMonth)
+                    .month(months.indexOf(month))
+                    .toDate();
+                  onMonthChange?.(newMonth);
+                }}
+              >
+                <SelectTrigger
+                  className={cn(
+                    buttonVariants({ variant: "ghost" }),
+                    "flex-1 justify-between",
+                  )}
+                >
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup className="flex flex-row w-[460px] flex-wrap">
+                    {/* <SelectLabel>Month</SelectLabel> */}
+                    {months.map((month) => (
+                      <SelectItem key={month} value={month} className="w-[115px]">
+                        {month}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <Select
+                value={currentYear}
+                onValueChange={(year) => {
+                  const newYear = dayjs(displayMonth).year(Number(year)).toDate();
+                  onMonthChange?.(newYear);
+                }}
+              >
+                <SelectTrigger
+                  className={cn(buttonVariants({ variant: "ghost" }), "w-[90px]")}
+                >
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Year</SelectLabel>
+                    {years.map((year) => (
+                      <SelectItem key={year} value={`${year}`}>
+                        {year}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+          );
+        },
+      }}
+      {...props}
+    />
+  );
+}
+Calendar.displayName = "Calendar";
+
+export { Calendar };
